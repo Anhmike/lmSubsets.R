5d3a89a6e9294cde18b89244e4af9fe488d05ac6
marc <marc.hofmann@gmail.com>
2020-06-24 09:20:19 +0200

    work in progress

--- tools/cranlogs.R
+++ tools/cranlogs.R
@@ -5,6 +5,9 @@ library("ggplot2")
 x <- cran_downloads("lmSubsets", from = "2018-03-24")
 
 if (!is.null(x)) {
+    op <- par(ask = TRUE)
+    on.exit(par(op))
+
     x <- x %>% mutate(year = format(date, "%Y"), month = format(date, "%m"))
     x <- x %>% group_by(package, year, month) %>% summarize(count = sum(count))
     x <- x %>% group_by(package) %>% mutate(total = cumsum(count))
@@ -16,13 +19,11 @@ if (!is.null(x)) {
     }))
     x <- as.data.frame(x %>% select(!c(year, month)))
 
-    readline("Monthly downloads [press Enter]:")
     z <- ggplot(x, aes(date, count, group=package, color=package)) +
-                geom_line() + geom_point(aes(shape=package))
+        geom_line() + geom_point(aes(shape=package)) + ggtitle("Monthly downloads")
     print(z)
 
-    readline("Total downloads [press Enter]:")
     z <- ggplot(x, aes(date, total, group=package, color=package)) +
-                geom_line() + geom_point(aes(shape=package))
+        geom_line() + geom_point(aes(shape=package)) + ggtitle("Total downloads")
     print(z)
 }
